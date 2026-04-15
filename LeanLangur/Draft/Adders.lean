dc6a5bbc13c7c7e8ccd4858bba9b00f61be86f42
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2026-04-15 19:04:12 +0530

    stubs

--- LeanLangur/Draft/Adders.lean
+++ LeanLangur/Draft/Adders.lean
@@ -1,15 +1,57 @@
 namespace langur
 
+#eval 3 + 4
+
+#eval "Hello, " ++ "world!"
+
+#eval [1, 2, 3] ++ [4, 5]
+
+#synth Add Nat
+
+open Add
+
+#eval add 3 7
+
+
+#check add
+
+/--
+error: failed to synthesize instance of type class
+  Add String
+
+Hint: Type class instance resolution failures can be inspected with the `set_option trace.Meta.synthInstance true` command.
+-/
+#guard_msgs in
+#eval add "Hello, " "world!"
+
+instance : Add String where
+  add x y := x ++ " " ++ y
+
+#eval add "Hello," "world!"
+
+/--
+error: failed to synthesize instance of type class
+  Add (?m.12 × ?m.14)
+
+Hint: Type class instance resolution failures can be inspected with the `set_option trace.Meta.synthInstance true` command.
+-/
+#guard_msgs in
+#eval add (3, 4) (5, 6)
+
+instance {α β : Type} [Add α ][Add β] : Add (α × β) where
+  add := fun (a, b) (c, d) ↦ (a + c, b + d)
+
+#eval (3, 4) + (5, 6)
+
+#eval (3, 4, 5) + (6, 7, 8)
+
+set_option pp.all true in
+#synth Add (Nat × Nat × String)
+
 instance : HAdd α (List α) (List α) where
   hAdd x xs := x :: xs
 
 #eval 3 + [4, 5]
 
-instance {α β : Type} [HAdd α α α][HAdd β β β] : HAdd (α × β) (α × β) (α × β) where
-  hAdd := fun (a, b) (c, d) ↦ (a + c, b + d)
-
-#eval (3, 4) + (5, 6)
-
-#eval (3, 4, 5) + (6, 7, 8)
 
 end langur
